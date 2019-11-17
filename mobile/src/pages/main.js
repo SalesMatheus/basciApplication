31069b4fb145d5e6078b86ccec96b15cf281a370
Matheus <matheus.carvalho.sales@gmail.com>
2019-11-17 15:45:26 -0300

    style page main

--- mobile/src/pages/main.js
+++ mobile/src/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import api from '../services/api';
 
-import { View, Text } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
 
 export default class Main extends Component {
     static navigationOptions = {
@@ -13,7 +13,7 @@ export default class Main extends Component {
     };
 
     state = {
-        products: [],
+        docs: [],
         productInfo: {},
         page: 1,
     };
@@ -27,21 +27,15 @@ export default class Main extends Component {
 
         const { docs, ...productInfo } = response.data;
 
-        this.setState({ products: docs, productInfo, page });
+        this.setState({
+            docs: [...this.state.docs, ...docs],
+            productInfo,
+            page
+        });
 
     };
 
-    prevPage = () => {
-        const { page } = this.state;
-
-        if (page === 1) return;
-
-        const pageNumber = page - 1;
-
-        this.loadProducts(pageNumber);
-    };
-
-    nextPage = () => {
+    loadMore = () => {
         const { page, productInfo } = this.state;
 
         if (page === productInfo.pages) return;
@@ -51,16 +45,79 @@ export default class Main extends Component {
         this.loadProducts(pageNumber);
     };
 
-    render() {
-        const { products, page, productInfo } = this.state;
+    renderItem = ({ item }) => (
+        <View style={styles.productContainer}>
+            <Text style={styles.productTitle}>{item.title}</Text>
+            <Text style={styles.productDescription}>{item.description}</Text>
 
+            <TouchableOpacity style={styles.productButton} onPress={() => { }}>
+                <Text style={styles.productButtonText}>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+    )
+
+    render() {
         return (
-            <View>
-                <Text>Hello World!!</Text>
-                {products.map(product => (
-                    <Text key={product._id}>{product.title}</Text>
-                ))}
+            <View style={styles.container}>
+                <FlatList
+                    contentContainerStyle={styles.list}
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                    onEndReached={this.loadMore}
+                    onEndReachedThreshold={0.1}
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fafafa"
+    },
+
+    list: {
+        padding: 20
+    },
+
+    productContainer: {
+        backgroundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: "#DDD",
+        borderRadius: 5,
+        padding: 20,
+        marginBottom: 20,
+    },
+
+    productTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#333"
+    },
+
+    productDescription: {
+        fontSize: 16,
+        color: "#999",
+        marginTop: 5,
+        lineHeight: 24,
+    },
+
+    productButton: {
+        height: 42,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#DA552F",
+        backgroundColor: "transparent",
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 12,
+    },
+
+    productButtonText: {
+        fontSize: 16,
+        color: '#DA552F',
+        fontWeight: 'bold',
+    }
+});
